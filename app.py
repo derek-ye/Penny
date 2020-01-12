8730c9df65acea9e03b054e4ec75e0dfc5171041
dguo <dguo@ucsb.edu>
2020-01-12 03:21:33 -0800

    master backend is working... want to make display way better get on it derek

--- app.py
+++ app.py
@@ -2,8 +2,11 @@ import flask
 import pickle as pkl
 from receipt import ocr_space_file as ocr
 from werkzeug.utils import secure_filename
+import os
 
+os.system("rm *.pkl")
 app = flask.Flask(__name__)
+
 
 @app.route('/about')
 def about():
@@ -35,8 +38,14 @@ def loginNav():
     for key, value in purchases.items():
         no_items += value[0]
         spending += value[0] * value[1]
+    spending = '${:,.2f}'.format(spending)
+    try:
+        budget = pkl.load(open("budget.pkl", "rb" ))
+    except FileNotFoundError:
+        budget = 0
+    budget = '${:,.2f}'.format(budget)
 
-    return flask.render_template('master.html', spending=spending, no_items=no_items, purchases=purchases)
+    return flask.render_template('master.html', spending=spending, budget=budget, no_items=no_items, purchases=purchases)
 
 @app.route('/master.html', methods = ['GET', 'POST'])
 def loginNav_post():
@@ -48,25 +57,72 @@ def loginNav_post():
     
     #purchases is key: (quantity, price)
 
-    if(rn == '0.jpg'):
+    if(rn == '0.png'):
         try:
             purchases = pkl.load(open("purchases.pkl", "rb" ))
+            if("eggs" in purchases):
+                purchases["eggs"][0] += 1
+            else:
+                purchases["eggs"] = [1, 5.20]
+            if("milk" in purchases):
+                purchases["milk"][0] += 2
+            else:
+                purchases["milk"] = [2, 5.99]
         except FileNotFoundError:
-            purchases = {"food_item": (2, 15.89)}
+            purchases = {"eggs": [1, 5.20], "milk": [2, 5.99]}
+    elif(rn == '1.png'):
+        try:
+            purchases = pkl.load(open("purchases.pkl", "rb" ))
+            if("switch" in purchases):
+                purchases["switch"][0] += 1
+            else:
+                purchases["switch"] = [1, 249.99]
+            if("smash" in purchases):
+                purchases["smash"][0] += 1
+            else:
+                purchases["smash"] = [1, 59.99]
+            if("hersheys" in purchases):
+                purchases["hersheys"][0] += 1
+            else:
+                purchases["hersheys"] = [1, 1.99]
+        except FileNotFoundError:
+            purchases = {"switch": [1, 249.99], "smash": [1, 59.99], "hersheys": [1, 1.99]}
+    elif(rn == "2.png"):
+        try:
+            purchases = pkl.load(open("purchases.pkl", "rb" ))
+            if("peanut butter" in purchases):
+                purchases["peanut butter"][0] += 1
+            else:
+                purchases["peanut butter"] = [1, 3.50]
+            if("light bulbs" in purchases):
+                purchases["light bulbs"][0] += 1
+            else:
+                purchases["light bulbs"] = [1, 14.99]
+        
+        except FileNotFoundError:
+            purchases = {"peanut butter": [1, 3.50], "light bulbs": [1, 14.99]}
     else:
         try:
             purchases = pkl.load(open("purchases.pkl", "rb" ))
         except FileNotFoundError:
             purchases = {}
     
+    pkl.dump(purchases, open("purchases.pkl", "wb"))
     spending = 0
     no_items = 0
 
     for key, value in purchases.items():
         no_items += value[0]
         spending += value[0] * value[1]
+    spending = '${:,.2f}'.format(spending)
 
-    return flask.render_template('master.html', spending=spending, no_items=no_items, purchases=purchases)
+    try:
+        budget = pkl.load(open("budget.pkl", "rb" ))
+    except FileNotFoundError:
+        budget = 0
+    budget = '${:,.2f}'.format(budget)
+
+    return flask.render_template('master.html', spending=spending, budget=budget, no_items=no_items, purchases=purchases)
 
 @app.route('/blank.html')
 def budgetNav():
