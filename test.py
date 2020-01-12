e740425e5d47dbea2b20272dbf26ff1985aa9906
Bryan Jang <bryanjang@ucsb.edu>
2020-01-11 22:16:32 -0800

    Updated OCR

--- test.py
+++ test.py
@@ -6,6 +6,6 @@ with PyTessBaseAPI() as api:
     for img in images:
         api.SetImageFile(img)
         print(api.GetUTF8Text())
-        print(api.AllWordConfidences())
+        # print(api.AllWordConfidences())
 # api is automatically finalized when used in a with-statement (context manager).
 # otherwise api.End() should be explicitly called when it's no longer needed.
